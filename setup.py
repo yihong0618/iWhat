d9fd6b43ecc5d820730af25debfe06b4f34f2e64
yihong0618 <zouzou0208@gmail.com>
2023-11-07 14:33:19 +0800

    fix: lock version

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author_email="zouzou0208@gmail.com",
     url="https://github.com/yihong0618/iWhat",
     license="MIT",
-    version="0.1.0",
-    install_requires=["rich", "openai"],
+    version="0.1.1",
+    install_requires=["rich", "openai==0.27.2"],
     entry_points={
         "console_scripts": ["iwhat = what.cli:main"],
     },
